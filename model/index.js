67f1ca357c0e73531d2c01a1624e54e74193ebde
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-12-20 00:34:01 +0200

    add mvp

--- model/index.js
+++ model/index.js
@@ -1,20 +1,84 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+import fs from 'fs/promises';
+import path from 'path';
+import { randomUUID } from 'crypto';
+import { fileURLToPath } from 'url';
 
-const listContacts = async () => {}
+import contacts from './contacts.json';
 
-const getContactById = async (contactId) => {}
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  return contacts;
+};
 
-const addContact = async (body) => {}
+const getContactById = async contactId => {
+  const contact = contacts.find(contact => contact.id === contactId);
+  return contact;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async contactId => {
+  console.log(contacts);
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if (index !== -1) {
+    contacts.splice(index, 1);
 
-module.exports = {
+    await fs.writeFile(
+      path.join(__dirname, 'contacts.json'),
+      JSON.stringify(contacts, null, 2),
+    );
+    return contacts;
+  }
+  return null;
+
+  /// НЕ РОЗУМІЮ ЧОМУ НАВЕДЕНИЙ НИЖЧЕ ВАРІАНТ НЕ ПРАЦЮЄ, ЗАМІНЮЄ КОНТАКТ ПОПЕРЕДНЬО ВИДАЛЕНИМ...В ПЕРШІЙ ДЗ ВСЕ ПРАЦЮВАЛО ОК...
+
+  // const contact = contacts.find(contact => contact.id === contactId);
+  // console.log(contact);
+  // if (!contact) {
+  //   return null;
+  // }
+  // const updContacts = contacts.filter(contact => contact.id !== contactId);
+  // console.log(updContacts);
+  // await fs.writeFile(
+  //   path.join(__dirname, 'contacts.json'),
+  //   JSON.stringify(updContacts, null, 2),
+  // );
+};
+
+const addContact = async ({ name, email, phone }) => {
+  if (name === undefined || email === undefined || phone === undefined) {
+    return null;
+  }
+  const newContact = { id: randomUUID(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(
+    path.join(__dirname, 'contacts.json'),
+    JSON.stringify(contacts, null, 2),
+  );
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  if (!body) {
+  }
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if (index !== -1) {
+    const updatedContact = { id: contactId, ...contacts[index], ...body };
+    console.log(updatedContact);
+    contacts[index] = updatedContact;
+    await fs.writeFile(
+      path.join(__dirname, 'contacts.json'),
+      JSON.stringify(contacts, null, 2),
+    );
+    return updatedContact;
+  }
+  return null;
+};
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,48 @@
-const express = require('express')
-const router = express.Router()
+import express from 'express';
+
+import model from '../../model/index.js';
+
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getContactById(id);
+  if (contact) {
+    return res.status(200).json(contact);
+  }
+
+  res.status(404).json({ message: 'Not found' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const newContact = await model.addContact(req.body);
+  if (newContact) {
+    return res.status(201).json(newContact);
+  }
+  res.status(400).json({ message: 'missing required name field' });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const removedContact = await model.removeContact(id);
+  if (removedContact) {
+    return res.status(200).json({ message: 'contact deleted' });
+  }
+  res.status(404).json({ message: 'Not found' });
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const updatedContact = await model.updateContact(id, req.body);
+  if (updatedContact) {
+    return res.status(200).json(updatedContact);
+  }
+  res.status(404).json({ message: 'Not found' });
+});
 
-module.exports = router
+export default router;
